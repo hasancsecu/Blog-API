7f17ee67eeb852bddf0bbc4c107f79489d0def2f
Abu Tamanna Hasan <tamanna.hasan@dohatec.com.bd>
2024-11-21 16:40:45 +0600

    dependency injection

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,37 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minNumbers: 1,
+      minSymbols: 1,
+      minUppercase: 1,
+    },
+    {
+      message:
+        'At least 8 characters, one symbol, one uppercase and one number',
+    },
+  )
+  password: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamsDto } from './dtos/get-users-params.dto';
+import { PatchUserDto } from './dtos/patch-user-dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('/:id?')
+  public GetUsers(
+    @Param() getUsersParamsDto: GetUsersParamsDto,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+  ) {
+    return getUsersParamsDto.id + ' -> ' + page + ' -> ' + limit;
+  }
+
+  @Post()
+  public CreateUser(@Body() createUserDto: CreateUserDto) {
+    return createUserDto;
+  }
+
+  @Patch()
+  public PatchUser(@Body() patchUserDto: PatchUserDto) {
+    return patchUserDto;
+  }
+}
